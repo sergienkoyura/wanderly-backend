d76687501cb35f358c2fca47f8ac2fb10afcb0c7
sergienkoyura <sergienkoyura5@gmail.com>
2025-04-29 15:49:23 +0300

    basic discovery service init

---
+++ discovery-service/src/test/java/com/wanderly/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wanderly.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
