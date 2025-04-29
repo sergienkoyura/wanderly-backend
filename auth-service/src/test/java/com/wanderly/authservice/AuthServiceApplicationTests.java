e612f7ae5285a26699b00de749c04988e892e63c
sergienkoyura <sergienkoyura5@gmail.com>
2025-04-29 15:54:08 +0300

    basic auth service init

---
+++ auth-service/src/test/java/com/wanderly/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderly.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
