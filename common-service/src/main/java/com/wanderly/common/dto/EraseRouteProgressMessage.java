25b226ab8b762954a9ec82d08082e740dac6acfa
sergienkoyura <sergienkoyura5@gmail.com>
2025-05-11 05:30:34 +0300

    completion event messages

---
+++ common-service/src/main/java/com/wanderly/common/dto/EraseRouteProgressMessage.java
@@ -0,0 +1,16 @@
+package com.wanderly.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EraseRouteProgressMessage {
+    private UUID routeId;
+}
